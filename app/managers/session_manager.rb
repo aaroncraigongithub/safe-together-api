17952ea0b1618a1f2967677795d689362cde2553
Aaron Craig <me@aaroncraig.com>
2016-11-14 10:58:35 -0800

    implement session manager

--- app/managers/session_manager.rb
+++ app/managers/session_manager.rb
@@ -3,6 +3,13 @@ class SessionManager
   class SessionManager::InvalidLogin < StandardError; end
 
   def self.create(email, password)
+    user = User.find_by email: email
+    raise SessionManager::InvalidLogin if user.nil?
+    raise SessionManager::InvalidLogin unless user.authenticate(password)
 
+    user.token = SecureRandom.hex
+    user.save!
+
+    user.token
   end
 end
---
+++ spec/managers/session_manager_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal:true
+require 'rails_helper'
+
+RSpec.describe SessionManager do
+  describe '#create' do
+    let(:create_session) { described_class.create(email, password) }
+    let(:password)       { Faker::Lorem.characters 10 }
+    let(:email)          { Faker::Internet.email }
+
+    context 'given a valid email/password' do
+      let(:email) { user.email }
+      let(:user)  { create(:user, password: password) }
+
+      it 'returns the user token' do
+        expect(create_session).to eq user.reload.token
+      end
+
+      it 'creates the user token' do
+        expect{ create_session }.to change{ user.reload.token }
+      end
+    end
+
+    context 'given an invalid email/password' do
+      it 'raises SessionManager::InvalidLogin' do
+        expect { create_session }.to raise_error(SessionManager::InvalidLogin)
+      end
+    end
+  end
+end
