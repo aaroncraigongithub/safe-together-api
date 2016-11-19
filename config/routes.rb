832f5344bb7e4edb9c585caa540d940e298bcd88
Aaron Craig <me@aaroncraig.com>
2016-11-18 16:31:24 -0800

    fix users/confirm route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   api_version(module: 'V1', path: { value: 'v1' }) do
     post '/users',                  to: 'users#create'
-    put  '/users/confirm/:token',   to: 'users#confirm'
+    put  '/users/confirm',          to: 'users#confirm'
     get  '/users',                  to: 'users#show'
 
     get  '/friends',                to: 'friends#show'
